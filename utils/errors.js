2a003ac4ae7fc75f32b759e5fc13100554aa35f5
DamariMorgado <dlmorgadomunoz@gmail.com>
2026-07-10 20:48:49 -0700

    added new error codes to utils and 2 new folder and files.

--- utils/errors.js
+++ utils/errors.js
@@ -1,12 +1,16 @@
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 const NOT_FOUND = 404;
+const CONFLICT = 409;
 const INTERNAL_SERVER_ERROR = 500;
 const CREATED = 201;
 const OK = 200;
 
 module.exports = {
   BAD_REQUEST,
+  UNAUTHORIZED,
   NOT_FOUND,
+  CONFLICT,
   INTERNAL_SERVER_ERROR,
   CREATED,
   OK,
